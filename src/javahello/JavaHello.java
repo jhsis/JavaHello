f72723f4f4f18383f612be4a432f8011347055ef
jhs <jhsis@shaw.ca>
2014-05-11 20:11:55 -0700

    Initial Commit

---
+++ src/javahello/JavaHello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package javahello;
+
+/**
+ *
+ * @author jhs
+ */
+public class JavaHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("hello world\n");
+    }
+}
